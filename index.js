04046b1dd7bc9e6dd8eb0d653a41f369f07fc20c
SC-YusepMuhammadZazuli <yusepmuhamadzazuli16@gmail.com>
2024-02-27 18:59:04 +0700

    feat

---
+++ index.js
@@ -0,0 +1,38 @@
+const qrcode = require('qrcode-terminal');
+
+const { Client, LocalAuth } = require('whatsapp-web.js');
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', (message) => {
+  console.log(message);
+});
+
+
+client.on('message', async (message) => {
+	if (message.body === 'halo aa') {
+		await client.sendMessage(message.from, 'alo dede nipai');
+	}
+});
+
+client.on('message', async (message) => {
+	if (message.body === 'sayang aku ga?') {
+		await client.sendMessage(message.from, 'sayang banget banget');
+	}
+});
+
+
+
+
+
+client.initialize();
